f293240a4e8966cdb51f48121207452ef3054d2a
chadmckellar <cdm090@usask.ca>
2024-12-02 19:32:49 -0700

    Day 2 Part 2

--- 2.cs
+++ 2.cs
@@ -13,30 +13,9 @@ public class Day2
         {
             var numbers = line.Split(" ").Select(x => int.Parse(x)).ToArray();
 
-            var i = 0;
-            if (numbers[i] > numbers[i + 1])
+            if (IsSafe(numbers))
             {
-                while (numbers[i] > numbers[i + 1] && Math.Abs(numbers[i] - numbers[i + 1]) <= 3)
-                {
-                    i++;
-                    if (i == numbers.Length - 1)
-                    {
-                        safeCount++;
-                        break;
-                    }
-                }
-            }
-            else if (numbers[i] < numbers[i + 1])
-            {
-                while (numbers[i] < numbers[i + 1] && Math.Abs(numbers[i] - numbers[i + 1]) <= 3)
-                {
-                    i++;
-                    if (i == numbers.Length - 1)
-                    {
-                        safeCount++;
-                        break;
-                    }
-                }
+                safeCount++;
             }
         }
 
@@ -45,7 +24,59 @@ public class Day2
 
     public static void Part2()
     {
+        var lines = File.ReadAllLines("2_input.txt");
 
+        var safeCount = 0;
+        foreach (var line in lines)
+        {
+            var numbers = line.Split(" ").Select(x => int.Parse(x)).ToArray();
+            if (IsSafe(numbers))
+            {
+                safeCount++;
+                continue;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var numbers_copy = numbers.Take(i).Concat(numbers.Skip(i + 1)).ToArray();
+                if (IsSafe(numbers_copy))
+                {
+                    safeCount++;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(safeCount);
+    }
+
+    public static bool IsSafe(int[] numbers)
+    {
+        var i = 0;
+        if (numbers[i] > numbers[i + 1])
+        {
+            while (numbers[i] > numbers[i + 1] && Math.Abs(numbers[i] - numbers[i + 1]) <= 3)
+            {
+                i++;
+                if (i == numbers.Length - 1)
+                {
+                    return true;
+                }
+            }
+        }
+        else if (numbers[i] < numbers[i + 1])
+        {
+            while (numbers[i] < numbers[i + 1] && Math.Abs(numbers[i] - numbers[i + 1]) <= 3)
+            {
+                i++;
+                if (i == numbers.Length - 1)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -4,3 +4,4 @@
 // Day1.Part2();
 
 Day2.Part1();
+Day2.Part2();
