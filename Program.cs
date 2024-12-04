d0df016ffa2bc3dd551d8c8a53b1721ce5814ec7
chadmckellar <cdm090@usask.ca>
2024-12-03 18:20:58 -0700

    Add timer

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// Run advent of code 2024 solutions
+var start = DateTime.Now;
 
 // Day1.Part1();
 // Day1.Part2();
@@ -6,5 +7,10 @@
 // Day2.Part1();
 // Day2.Part2();
 
-// Day3.Part1();
-Day3.Part2();
+
+Day3.Part1();
+// Day3.Part2();
+
+
+var end = DateTime.Now;
+Console.WriteLine($"Time: {end - start}");
