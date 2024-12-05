9d42222f4800269b72d343946f14038858a6064f
chadmckellar <cdm090@usask.ca>
2024-12-04 22:58:47 -0700

    Day 5 Part 2

--- 5.cs
+++ 5.cs
@@ -32,49 +32,77 @@ public class Day5
         var lines2 = File.ReadAllLines("5_input_2.txt");
         var rows = lines2.Select(line => line.Split(',').Select(int.Parse).ToArray()).ToArray();
 
-        int sum = 0;
+        int sumPart1 = 0;
+        int sumPart2 = 0;
         foreach (var row in rows)
         {
-            int i = 0;
+            int i = 1;
             bool valid = true;
-            foreach (var num in row)
+            foreach (var num in row.Take(row.Length - 1).Skip(1))
             {
                 if (dict1.ContainsKey(num))
                 {
                     // dict 1 has a key of num, where the values cannot appear before it
-                    foreach (var n in row.Take(i))
+                    int n = row[i - 1];
+                    if (dict1[num].Contains(n))
                     {
-                        if (dict1[num].Contains(n))
-                        {
-                            valid = false;
-                            break;
-                        }
+                        valid = false;
                     }
                 }
                 if (dict2.ContainsKey(num))
                 {
                     // dict 2 has a key of num, where the values cannot appear after it
-                    foreach (var n in row.Skip(i))
+                    int n = row[i + 1];
+                    if (dict2[num].Contains(n))
                     {
-                        if (dict2[num].Contains(n))
-                        {
-                            valid = false;
-                            break;
-                        }
+                        valid = false;
                     }
                 }
+                if (!valid)
+                    break;
                 i++;
             }
             if (valid)
-                sum += row[i / 2];
-        }
-        Console.WriteLine(sum);
-    }
+                sumPart1 += row[i / 2];
+            else
+            {
+                // order the row according to the dict ordering rules
+                var orderedRow = new List<int>(row);
+                bool changed;
+                do
+                {
+                    changed = false;
+                    for (int j = 0; j < orderedRow.Count - 1; j++)
+                    {
+                        int a = orderedRow[j];
+                        int b = orderedRow[j + 1];
 
+                        // Check if b must come before a (dict1)
+                        if (dict1.ContainsKey(a) && dict1[a].Contains(b))
+                        {
+                            orderedRow[j] = b;
+                            orderedRow[j + 1] = a;
+                            changed = true;
+                        }
+                        // Check if a must come before b (dict2)
+                        else if (dict2.ContainsKey(b) && dict2[b].Contains(a))
+                        {
+                            orderedRow[j] = b;
+                            orderedRow[j + 1] = a;
+                            changed = true;
+                        }
+                    }
+                } while (changed);
+
+                sumPart2 += orderedRow[orderedRow.Count / 2];
+            }
+        }
+        Console.WriteLine(sumPart1);
+        Console.WriteLine(sumPart2);
+    }
 
     public static void Part2()
     {
-
     }
 
 }
