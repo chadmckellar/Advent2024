3741ffb53c4de79b16129b3b631ceb0990c99c44
chadmckellar <cdm090@usask.ca>
2024-12-04 21:32:52 -0700

    Add input to select which day to run

---
+++ 5.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+public class Day5
+{
+    public static void Part1()
+    {
+
+    }
+
+
+    public static void Part2()
+    {
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,43 @@
 ﻿// Run advent of code 2024 solutions
+Console.WriteLine("Enter the day number (1-25):");
+if (!int.TryParse(Console.ReadLine(), out int day) || day < 1 || day > 25)
+{
+    Console.WriteLine("Invalid input. Please enter a number between 1 and 25.");
+    return;
+}
+
 DateTime start = DateTime.Now;
 
-// Day1.Part1();
-// Day1.Part2();
+try
+{
+    // Get the type for the requested day
+    Type? dayType = Type.GetType($"Day{day}");
+    if (dayType == null)
+    {
+        Console.WriteLine($"Day {day} not implemented yet.");
+        return;
+    }
 
-// Day2.Part1();
-// Day2.Part2();
+    // Call Part1 and Part2 methods
+    var part1Method = dayType.GetMethod("Part1");
+    var part2Method = dayType.GetMethod("Part2");
 
-// Day3.Part1();
-// Day3.Part2();
-
-Day4.Part1();
-Day4.Part2();
+    if (part1Method != null && part2Method != null)
+    {
+        part1Method.Invoke(null, null);
+        part2Method.Invoke(null, null);
+    }
+    else
+    {
+        Console.WriteLine($"Day {day} methods not found.");
+        return;
+    }
+}
+catch (Exception)
+{
+    Console.WriteLine($"Day {day} not implemented yet.");
+    return;
+}
 
 DateTime end = DateTime.Now;
-Console.WriteLine($"Time: {end - start}");
-
+Console.WriteLine($"Time for Day {day}: {end - start}");
