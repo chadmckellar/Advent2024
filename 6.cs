0fbd60a01c8906dbe45df087a1af52635c3bcf76
chadmckellar <cdm090@usask.ca>
2024-12-07 06:40:08 -0700

    Day 6 Part 2

--- 6.cs
+++ 6.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,6 @@ public class Day6
     {
         var lines = File.ReadAllLines("6_input.txt");
         var grid = lines.Select(line => line.ToCharArray()).ToArray();
-
-        // directions N, E, S, W
-        int[] dx = { -1, 0, 1, 0 };
-        int[] dy = {  0, 1, 0, -1 };
-
-        int dir = 0;
 
         // find ^ in grid
         int x = 0, y = 0;
@@ -34,31 +29,76 @@ public class Day6
         }
 
         HashSet<(int, int)> visited = [(x, y)];
+        HashSet<(int, int)> loopObstructions = new HashSet<(int, int)>();
 
-        bool reachedEnd = false;
-        do {
-            int rd = x + dx[dir];
-            int cd = y + dy[dir];
-            if (rd < 0 || rd >= grid.Length || cd < 0 || cd >= grid[0].Length)
-            {
-                reachedEnd = true;
-            }
-            else if (grid[rd][cd] == '#')
-            {
-                turnRight(ref dir);
-                rd = x + dx[dir];
-                cd = y + dy[dir];
-            }
-            visited.Add((x, y));
-            x = rd;
-            y = cd;
-        } while (reachedEnd is false);
+        int totalSteps = RunTraversal(grid, x, y, visited, loopObstructions);
+        Console.WriteLine($"Part1: {visited.Count}");
 
-        Console.WriteLine(visited.Count);
+        foreach (var visit in visited.Skip(1))
+        {
+            RunTraversal(grid, x, y, new HashSet<(int, int)>(), loopObstructions, addedObstruction: visit);
+        }
+        Console.WriteLine($"Part2: {loopObstructions.Count}");
     }
 
     private static void turnRight(ref int dir)
     {
         dir = (dir + 1) % 4;
+    }
+
+    private static int RunTraversal(char[][] grid, int x, int y, HashSet<(int, int)> visited,  HashSet<(int, int)> loopObstructions, (int, int)? addedObstruction = null)
+    {
+        var localGrid = grid.Select(row => row.ToArray()).ToArray();
+        if (addedObstruction != null)
+        {
+            localGrid[addedObstruction.Value.Item1][addedObstruction.Value.Item2] = '#';
+        }
+        // directions N, E, S, W
+        int[] dx = { -1, 0, 1, 0 };
+        int[] dy = {  0, 1, 0, -1 };
+
+        int dir = 0;
+
+        int step = 0;
+        HashSet<(int, int, int)> loopCheck = new HashSet<(int, int, int)>(); // (x, y, dir)
+
+        bool reachedEnd = false;
+        while (reachedEnd is false)
+        {
+            // check if loop
+            if (loopCheck.Contains((x, y, dir)))
+            {
+                Debug.Assert(addedObstruction != null);
+                loopObstructions.Add(addedObstruction.Value);
+                reachedEnd = true;
+            }
+            loopCheck.Add((x, y, dir));
+
+            // grab intended next position
+            int rd = x + dx[dir];
+            int cd = y + dy[dir];
+
+            if (rd < 0 || rd >= localGrid.Length || cd < 0 || cd >= localGrid[0].Length)
+            {
+                reachedEnd = true;
+            }
+            else if (localGrid[rd][cd] == '#')
+            {
+                while (localGrid[rd][cd] == '#') // turn right until no obstruction
+                {
+                    turnRight(ref dir);
+                    rd = x + dx[dir];
+                    cd = y + dy[dir];
+                }
+            }
+
+            visited.Add((x, y));
+            // move in the direction the guard is facing
+            x = x + dx[dir];
+            y = y + dy[dir];
+            step++;
+        }
+
+        return step;
     }
 }
